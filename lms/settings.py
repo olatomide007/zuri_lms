b43c1fd743e659db6da6b6e1c605e44cd753b084
olatomide007 <olatomideafolabi@gmail.com>
2022-07-15 06:16:06 -0700

    lms: polls: Create a new app called polls

--- lms/settings.py
+++ lms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls', #or 'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = True    #you can change to false if you changeing to Africa/Lagos in te Time Zone.
 
 
 # Static files (CSS, JavaScript, Images)
---
+++ polls/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    #number
+    #name
+    #pub.date
+    #no need for this line of code cos django does the auto for us.
+    question_text = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+# 4)                   "Are you happy about Djagno"                                     "June 16, 09:50 WAT"
+# 5)                   "ARE YOU .."                                                    "JUNE 17, 10:00 WAT"
+
+
+class Choices(models.Model):
+    #number of choice
+    # link to a question
+    # choice_of_answer           [yes/no]
+    # votes
+
+   #django has done the auto id
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_answer = models.CharField(max_length=20)
+    vote = models.IntegerField(default=0)
+
+
+# 1)                  "Are you happy about Djagno"                yes/no              3/0
+
+
